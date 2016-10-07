bb1fd8ac4619f4b835151815f473341feb3cea0e
王晨 <wangchen45_love@126.com>
2016-10-07 21:38:32 +0800

    socket io

--- nodeJs/聊天/lib/chat_server.js
+++ nodeJs/聊天/lib/chat_server.js
@@ -1,3 +1,25 @@
 /**
  * Created by Administrator on 2016/10/5.
  */
+const scoketio=require("socket.io");
+var io;
+var guestNumber=1;
+var nikeNames={};
+var namesUsed=[];
+var currentRoom{};
+
+exports.listen=function(server){
+    io=scoketio.listen(server0)
+    io=set("log leve1",1);
+    io.scokets.on("connection",funcion(){
+        guestNumber=assignGuestName(socket,guestNumber,nikeName,namesUsed)
+        joinRoom(socket,"lobby");
+        handleMessageBroadcasting(socket,nikeName);
+        handleNameChangeAttempts(socket,nikeName,nameUsed);
+        handleRoomJoining(socket);
+        socket.on("rooms",function(){
+            socket.emit("rooms",io.scokets.manager.rooms);
+        });
+        handleClientDisconnection(scoket,nikeName,nameUsed);
+    });
+}
--- nodeJs/聊天/server.js
+++ nodeJs/聊天/server.js
@@ -1,3 +1,65 @@
 /**
  * Created by Administrator on 2016/10/5.
  */
+
+const http=require("http"),
+       fs=require("fs"),
+       path=require("path"),
+       mime=require("mime");
+var cache={};
+
+var chatServer=request("./lib/chat_server");
+chatServer.listen(server);
+
+function send404(response){
+    response.writeHead(404,{"Content-Type":"text/plan"});
+    response.write("error 404:response not found");
+    response.end();
+}
+function sendFile(response,filePath,fileContents){
+    response.writeHead(
+        200,
+        {"Content-Type":mime.lookup(path.basename(filePath))}
+    )
+    response.end(fileContents);
+}
+function serveStatic(response,cache,absPath){
+    if(cache[absPath]){
+        sendFile(response,absPath,cache[absPath]);
+    }else{
+        fs.exists(absPath,function(exsits){
+            if(exsits){
+                fs.readFile(absPath,function(err,data){
+                    if(err){
+                        send404(response)
+                    }else{
+                        cache[absPath]=data;
+                        sendFile(response,absPath,data);
+                    }
+                });
+            }else{
+                send404(respons);
+            }
+        })
+    }
+}
+
+var server=http.createServer(function(request,response){
+    var filePath=false;
+    if(request.url=="/"){
+        filePath="public/index.html"
+    }else{
+        filePath="pubilc/"+request.url;
+    }
+    var absPath="./"+filePath;
+    serveStatic(response,cache,absPath);
+});
+server.listen(8080,function(){
+    console.log(111)
+})
+
+
+
+
+
+
