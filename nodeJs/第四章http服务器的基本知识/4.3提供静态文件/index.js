e0970262637e140dd0a77c1aa9b2ead9c26b68e7
unknown <wangchen8@360buyAD.local>
2016-10-19 20:05:09 +0800

    csssssss

--- nodeJs/第四章http服务器的基本知识/4.3提供静态文件/index.js
+++ nodeJs/第四章http服务器的基本知识/4.3提供静态文件/index.js
@@ -1,3 +1,25 @@
 /**
  * Created by richu on 2016/10/19.
  */
+
+var http=require("http"),
+    parse=require("url").parse,
+    join=require("path").join;
+    fs=require("fs");
+
+var root=__dirname;
+
+http.createServer(function(resquest,response){
+
+    var url=parse(resquest.url);
+    var path=join(root,url.pathname);
+    //ÎÄ¼şÁ÷
+    var stream=fs.createReadStream(path);
+
+    stream.on("data",function(chunk){
+
+    });
+    stream.on("end",function(){
+        response.end();
+    })
+}).listen(8080);
