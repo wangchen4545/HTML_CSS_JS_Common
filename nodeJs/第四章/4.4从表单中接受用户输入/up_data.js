b8d95015efa73ca79b3ab0d308779f06612bcfab
unknown <wangchen8@360buyad.local>
2016-10-26 21:47:03 +0800

    note

--- nodeJs/第四章/4.4从表单中接受用户输入/up_data.js
+++ nodeJs/第四章/4.4从表单中接受用户输入/up_data.js
@@ -3,7 +3,7 @@
  */
 
 var http=require("http");
-var form=require("formidable");
+var formidable=require("formidable");
 
 var server=http.createServer(function(request,response){
 
@@ -14,26 +14,56 @@ var server=http.createServer(function(request,response){
         case "POST":
             up_data(request,response);
             break;
-
     }
-
-});
+}).listen(8080);
 function show(request,response){
-    var html='<form action="\" method="post" enctype="multipart/form-data"> <p> <input type="text" name="name"/> </p> <p> <input type="file" name="file"/> </p> <p><input type="submit" value="ÉÏ‚÷"/></p> </form>';
+    var html='<form action="\" method="post" enctype="multipart/form-data"> <p> <input type="text" name="name"/> </p> <p> <input type="file" name="file"/> </p> <p><input type="submit" value="ÉÏ´«"/></p> </form>';
     response.setHeader("Content-Type","text/html");
-    response.end(html)
+    response.end(html);
 }
 function up_data(request,response){
     if(!isformData(request)){
         response.statusCode=400;
-        response.end("404")
+        response.end("404");
         return;
     }
+    var form=new formidable.IncomingForm();
+    var mydate=new Date().getTime();
+    form.on("field",function(field,value){
+        //console.log(field);
+        //console.log(value);
+    });
+
+    form.on("file",function(name,file){
+        //console.log("name----",name);
+        //console.log(file);
+    });
+
+    form.on("end",function(){
+        response.end("updata_complete");
+
+        console.log("currentTime"+mydate);
+    });
+    form.parse(request);
 }
 
 function isformData(request){
-    var type=request.headers["Content-type"] || "";
+    var type=request.headers["content-type"] || " ";
+    console.log(request.headers["content-type"]);
 
     return 0==type.indexOf("multipart/form-data");
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
