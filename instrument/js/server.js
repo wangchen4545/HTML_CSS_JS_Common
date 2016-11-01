4f97824bb2ff3e743c8f5a9ef77975b26f18ac5e
unknown <wangchen8@360buyAD.local>
2016-11-01 21:39:01 +0800

    2016.11.01 创建node

---
+++ instrument/js/server.js
@@ -0,0 +1,37 @@
+/**
+ * Created by richu on 2016/11/1.
+ */
+var http=request("http");
+var fs=request("fs");
+var server=http.createServer(function(request,response){
+
+
+
+    fs.readFile("../test.html",function(err){
+
+        request.set
+
+    })
+
+
+
+}).listen(808);
+
+
+defer.then(function () {
+    this.jsToNaWeiXin({
+        isShow: true,
+        optionType: 1,
+        btnText: '分享',
+        shareDate: {
+            appId: '',
+            img: 'http://m.360buyimg.com/mobilecms/jfs/t3781/260/864778713/5597/e681bebf/581742f2N94841359.jpg',
+            link: 'http://sale.jd.com/m/act/Mfa5juye82.html',
+            desc: '金融双11·11强势来袭，抢千万品类专享优惠券，好货打白条高达12期免息，速来抢！',
+            title: '金融双11·11强势来袭，抢千万品类专享优惠券，好货打白条高达12期免息，速来抢！',
+            friendesc: '金融双11·11强势来袭，抢千万品类专享优惠券，好货打白条高达12期免息，速来抢！',
+            type: ''
+        }
+    });
+});
+
