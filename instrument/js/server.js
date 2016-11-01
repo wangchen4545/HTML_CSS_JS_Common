ce454b182eca21ef3d553c567f5fd492f3acc542
王晨 <wangchen45_love@126.com>
2016-11-02 00:03:09 +0800

    create server

--- instrument/js/server.js
+++ instrument/js/server.js
@@ -1,37 +1,22 @@
 /**
  * Created by richu on 2016/11/1.
  */
-var http=request("http");
-var fs=request("fs");
+var http=require("http");
+var fs=require("fs");
+var str="<ul><li>1</li></ul>";
 var server=http.createServer(function(request,response){
 
+    fs.readFile("../test.html",function(err,data){
 
+        request.setEncoding("utf8");
+        //request.statusCode=200;
+        response.writeHead(200,{"Set-cookie":"sid=wangchen"});
+        var str=data.toString();
 
-    fs.readFile("../test.html",function(err){
+        response.end(str);
 
-        request.set
-
+        console.log(str);
     })
 
-
-
-}).listen(808);
-
-
-defer.then(function () {
-    this.jsToNaWeiXin({
-        isShow: true,
-        optionType: 1,
-        btnText: '分享',
-        shareDate: {
-            appId: '',
-            img: 'http://m.360buyimg.com/mobilecms/jfs/t3781/260/864778713/5597/e681bebf/581742f2N94841359.jpg',
-            link: 'http://sale.jd.com/m/act/Mfa5juye82.html',
-            desc: '金融双11·11强势来袭，抢千万品类专享优惠券，好货打白条高达12期免息，速来抢！',
-            title: '金融双11·11强势来袭，抢千万品类专享优惠券，好货打白条高达12期免息，速来抢！',
-            friendesc: '金融双11·11强势来袭，抢千万品类专享优惠券，好货打白条高达12期免息，速来抢！',
-            type: ''
-        }
-    });
-});
+}).listen(8080);
 
